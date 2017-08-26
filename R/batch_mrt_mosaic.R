db09c7d18877246a4a53cc967e80fa5395efe9bf
Fan Hongxiang (NIGLAS) <fanhongxiang13@mails.ucas.ac.cn>
2017-08-26 10:33:18 +0800

    add .parallel

--- R/batch_mrt_mosaic.R
+++ R/batch_mrt_mosaic.R
@@ -9,7 +9,9 @@
 # hdf_date represents the study period, which is expected to have the same length as the final tif files.
 # raw_hdf_path is the path where the original MOD16 HDF files are stored.
 # mosaic_hdf_path is the path to store the mosaic HDF files
-
+#' @importFrom foreach %dopar%
+#' @importFrom foreach %do%
+NULL
 #' Mosiac a series of MODSI satellite images using MRT.
 #' @param hdf_date The date of the MODIS satellite image.
 #' Can be obtained from the \code{\link{get_hdf_dates}} function.
@@ -19,6 +21,9 @@
 #' Pass to \code{\link{mosaic_modis_hdf}} function.
 #' @param delete Logical. If TRUE the original hdf files will be deleted after mosaicing. Default is FALSE.
 #' Pass to \code{\link{mosaic_modis_hdf}} function.
+#' @param .parallel Logical. Should mosaic be executed in parallel mode?
+#' @param nCores Numeric. The number of clusters to use when \code{.parallel = T}.
+#'
 #' @description Runs \code{\link{mosaic_modis_hdf}} on a batch of files.
 #' It internally use MODIS Reproject Tool (MRT) software to mosaic the downloaded images.
 #' @note To have the functionality for Mosaic and reprojecting of the images,
@@ -27,16 +32,41 @@
 #' mrt_path argument.
 #' @seealso \code{\link{mosaic_modis_hdf}} \code{\link{reproject_modis_hdf}} \code{\link{get_hdf_dates}}
 #' @export
-batch_mrt_mosaic <- function(hdf_date,
-                             raw_hdf_path,
+batch_mrt_mosaic <- function(raw_hdf_path,
                              mosaic_hdf_path,
                              bands_subset = "",
-                             delete = FALSE){
-  temp.hdf.list <- list.files(raw_hdf_path,
-                              pattern = hdf_date,
-                              full.names = T)
-  mosaic_modis_hdf(hdf_fpnames = temp.hdf.list,
-                   dst_fpname = file.path(mosaic_hdf_path, paste0(hdf_date, ".hdf")),
-                   bands_subset = bands_subset,
-                   delete = delete)
+                             delete = FALSE,
+                             .parallel = TRUE,
+                             nCores = 4){
+  hdf_dates <- modisr::get_hdf_dates(raw_hdf_path)
+  if(.parallel){
+    message("Run in parallel mode:")
+    cl <- modisr::create_snow_cluster(nCores)
+    pb <- utils::txtProgressBar(max = length(hdf_dates), style = 3)
+    progress <- function(n) utils::setTxtProgressBar(pb, n)
+    opts <- list(progress = progress)
+    foreach::foreach(hdf_date = hdf_dates,
+                     .options.snow = opts) %dopar% {
+                       mosaic_modis_hdf(hdf_fpnames = list.files(raw_hdf_path,
+                                                                 pattern = hdf_date,
+                                                                 full.names = T),
+                                        dst_fpname = file.path(mosaic_hdf_path, paste0(hdf_date, ".hdf")),
+                                        bands_subset = bands_subset,
+                                        delete = delete)
+                     }
+    on.exit({
+      snow::stopCluster(cl)
+      close(pb)
+      })
+  } else{
+    foreach::foreach(hdf_date = hdf_dates) %do% {
+      mosaic_modis_hdf(hdf_fpnames = list.files(raw_hdf_path,
+                                                pattern = hdf_date,
+                                                full.names = T),
+                       dst_fpname = file.path(mosaic_hdf_path, paste0(hdf_date, ".hdf")),
+                       bands_subset = bands_subset,
+                       delete = delete)
+    }
+  }
+
 }
